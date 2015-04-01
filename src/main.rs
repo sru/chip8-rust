bc5ddc8e54f18a806da23b53c3b25f41d38fb3c9
SungRim Huh <sungrimhuh@gmail.com>
2015-03-31 19:14:07 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,27 @@
+extern crate chip8;
+
+use std::io::prelude::*;
+use std::fs::File;
+
+use chip8::Chip8;
 fn main() {
+  let args: Vec<_> = std::env::args().collect();
+  if args.len() != 2 {
+    println!("Usage: {} program", args[0]);
+    return;
+  }
+
+  let mut file = match File::open(&args[1]) {
+    Ok(file) => file,
+    Err(e) => panic!("Could not open file {}: {}", args[1], e),
+  };
+
+  let mut buf = Vec::new();
+  match file.read_to_end(&mut buf) {
+    Ok(_) => {},
+    Err(e) => panic!("Could not read file {}: {}", args[1], e),
+  };
+
+  let mut chip8 = Chip8::new();
+  chip8.load(&buf);
 }
